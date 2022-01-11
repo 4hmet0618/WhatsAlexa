7d77ef31625de9ee89be7d509ad163661f453c5c
TOXIC DEVIL <toxicdevil.abhinav@gmail.com>
2022-01-11 14:32:52 +0530

    Delete termux.sh

--- termux.sh
+++
@@ -1,8 +0,0 @@
-apt update
-apt install nodejs
-apt install git
-git clone https://github.com/TOXIC-DEVL/WhatsAlexa
-cd WhatsAlexa
-npm install @adiwajshing/baileys
-npm install chalk
-node index.js
